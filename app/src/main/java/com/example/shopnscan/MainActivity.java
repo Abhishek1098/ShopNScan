f6db06fa37503d9bf2bbd2e3e0edaf13e504718b
Shiven Kumar <kumar.shiven15@gmail.com>
2019-04-06 15:21:18 -0400

    ListView and 3 buttons added to MainActivity

--- app/src/main/java/com/example/shopnscan/MainActivity.java
+++ app/src/main/java/com/example/shopnscan/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.shopnscan;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView cartList;
+    private Button checkoutButton, scanButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //commit
-        //secondary
-        //hello
+        cartList = (ListView)findViewById(R.id.MainActivity_ListView_Cart);
+        checkoutButton = (Button)findViewById(R.id.MainActivity_Button_Checkout);
+        scanButton = (Button)findViewById(R.id.MainActivity_Button_Scan);
     }
 }
